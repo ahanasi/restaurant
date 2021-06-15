3208061ebf040a7ada86f8cef553fa9ca1014e41
XXX Ahana <ahanasi@seas.upenn.edu>
2021-06-14 21:17:16 -0700

    Create tab-switching logic

--- src/hours.js
+++ src/hours.js
@@ -1,11 +1,8 @@
 const hoursLoc = () => {
-  const contentDiv = document.createElement("div");
-  const overlay = document.createElement("div");
-  const headline = document.createElement("header");
-  const mainContent = document.createElement("section");
 
-  const h1 = document.createElement("h1");
-  h1.textContent = "rasa";
+  const contentDiv = document.querySelector(".content")
+  const overlay = document.querySelector(".overlay");
+  const mainContent = document.createElement("section");
 
   const h2 = document.createElement("h2");
   h2.textContent = "hours & location";
@@ -13,8 +10,9 @@ const hoursLoc = () => {
   const locWrapper = document.createElement("p");
   const address = document.createElement("a");
   const tel = document.createElement("a");
+  const days = document.createElement("p");
 
-  locWrapper.classList.add("locWrapper")
+  locWrapper.classList.add("locWrapper");
 
   address.classList.add("address", "flex-col", "text-center");
   tel.classList.add("address", "flex-col", "text-center");
@@ -26,19 +24,26 @@ const hoursLoc = () => {
   tel.textContent = "650-340-7272";
   tel.href = "tel:650-340-7272";
 
-  contentDiv.classList.add("content");
-  overlay.classList.add("overlay");
-  headline.classList.add("headline");
-  mainContent.classList.add("flex-col", "all-center");
+  days.innerHTML = `<strong>TUES - SUNDAY</strong><br> 
+  <strong>LUNCH:</strong> (Takeout Only)<br>
+  12:00 PM - 3:00PM<br>
+  <strong>BRUNCH: (Sat - Sun)</strong><br>
+  <strong>NOW even Saturday - Starting 04/02/21</strong><br>
+  12:00 PM - 3:00PM<br>
+  <strong>DINNER:</strong><br>
+  4:30 PM - 8:30PM<br>
+  <strong>FRI - SAT:</strong><br>
+  4:30 PM - 9:30PM<br>`;
 
-  headline.append(h1);
+  mainContent.classList.add("flex-col", "all-center");
+  days.classList.add("text-center");
 
   mainContent.append(h2);
   locWrapper.append(address);
   locWrapper.append(tel);
+  locWrapper.append(days);
   mainContent.append(locWrapper);
 
-  overlay.append(headline);
   overlay.append(mainContent);
   contentDiv.append(overlay);
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,49 @@
 import "./style.sass";
 import "./reset.sass";
 import homePage from "./home";
-import hoursLoc from "./hours"
+import hoursLoc from "./hours";
+import { list } from "postcss";
 
-console.log("Page has loaded!");
+const bgHeader = () => {
+  const contentDiv = document.createElement("div");
+  const overlay = document.createElement("div");
+  const headline = document.createElement("header");
+  const navbar = document.createElement("nav");
+  const navlist = document.createElement("ul");
+  const navItems = [
+    ["home", homePage],
+    ["hours & location", hoursLoc],
+  ];
+  const h1 = document.createElement("h1");
 
-document.body.appendChild(hoursLoc());
+  h1.textContent = "rasa";
+
+  contentDiv.classList.add("content");
+  overlay.classList.add("overlay");
+  headline.classList.add("headline");
+
+  navItems.forEach(([text, fnCall]) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = text;
+    listItem.addEventListener("click", () => {
+      contentDiv.remove();
+      document.body.appendChild(bgHeader());
+      document.body.appendChild(fnCall());
+    });
+    navlist.append(listItem);
+  });
+
+  navbar.classList.add("topnav");
+  navbar.appendChild(navlist);
+
+  headline.append(h1);
+  headline.appendChild(navbar);
+
+  overlay.append(headline);
+  contentDiv.append(overlay);
+
+  return contentDiv;
+};
+
+document.body.appendChild(bgHeader());
+document.body.appendChild(homePage());
